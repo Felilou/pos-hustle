db2fe4885a57e5a2541ac40ed1d85236e7a6f93c
Joachim Grueneis (Spengergasse) <grueneis@spengergasse.at>
2025-03-13 16:29:02 +0100

    Renamed directory of test projects according to TT

--- HIF_FT_dotnet/create-empty-project.sh
+++ HIF_FT_dotnet/create-empty-project.sh
@@ -9,9 +9,9 @@ dotnet new sln \
 mkdir -p src/Aufgabe1_ORMapping
 mkdir -p src/Aufgabe2_Services
 mkdir -p src/Aufgabe3_RestfulApi
-mkdir -p test/Aufgabe1_ORMapping.Test
-mkdir -p test/Aufgabe2_Services.Test
-mkdir -p test/Aufgabe3_RestfulApi.Test
+mkdir -p tests/Aufgabe1_ORMapping.Test
+mkdir -p tests/Aufgabe2_Services.Test
+mkdir -p tests/Aufgabe3_RestfulApi.Test
 
 dotnet new classlib \
   --name Aufgabe1_ORMapping \
@@ -29,14 +29,14 @@ dotnet sln add src/Aufgabe3_RestfulApi
 
 dotnet new xunit \
   --name Aufgabe1_ORMapping.Test \
-  --output test/Aufgabe1_ORMapping.Test
+  --output tests/Aufgabe1_ORMapping.Test
 dotnet new xunit \
   --name Aufgabe2_Services.Test \
-  --output test/Aufgabe2_Services.Test
+  --output tests/Aufgabe2_Services.Test
 dotnet new xunit \
   --name Aufgabe3_RestfulApi.Test \
-  --output test/Aufgabe3_RestfulApi.Test
+  --output tests/Aufgabe3_RestfulApi.Test
 
-dotnet sln add test/Aufgabe1_ORMapping.Test
-dotnet sln add test/Aufgabe2_Services.Test
-dotnet sln add test/Aufgabe3_RestfulApi.Test
+dotnet sln add tests/Aufgabe1_ORMapping.Test
+dotnet sln add tests/Aufgabe2_Services.Test
+dotnet sln add tests/Aufgabe3_RestfulApi.Test
