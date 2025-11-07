1543ba2f23d02d799b3b1fbb6b98e8a9104e3c73
Joachim Grüneis <grueneis@spengergasse.at>
2025-11-07 17:09:34 +0100

    Upgrading github actions (#57)
    * Upgrading github actions
    
    * Adapted pom files for java 25
    
    * upgraded gradle version for kotlin
    
    * dotnet quality needs to be ga
    
    * Update Gradle build command to write info messages
    
    * github action for gradle
    
    # Conflicts:
    #	.github/workflows/kotlin-spring.yml
    
    * specify vendor for java toolchain
    
    ---------
    
    Co-authored-by: Joachim Grüneis <joachim@grueneis.at>

--- HIF_FT_kotlin/Aufgabe2_BusinessService/build.gradle.kts
+++ HIF_FT_kotlin/Aufgabe2_BusinessService/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.1.21"
-    kotlin("plugin.spring") version "2.1.21"
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.spring") version "2.2.20"
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "2.1.21"
@@ -10,14 +10,15 @@ apply(from = rootProject.file("buildSrc/shared.gradle.kts"))
 val springCloudVersion by extra("2024.0.1")
 
 kotlin {
-    jvmToolchain(23)
+    jvmToolchain(24)
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
 }
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(23)
+        languageVersion = JavaLanguageVersion.of(24)
+        vendor = JvmVendorSpec.ADOPTIUM
     }
 }
 
